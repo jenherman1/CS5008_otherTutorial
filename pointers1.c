9d7f5327618c39e9deb5f5ce3eed43c75baba93c
jenherman1 <jennifer.h.seitzer@gmail.com>
2022-04-14 13:11:00 -0700

    adding pointers code

---
+++ pointers1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+void update(int myVal){
+     myVal = 10;
+}// function UPDATE
+
+
+//--------------------------------------------------
+int main() {
+     int   A = 5;
+     int   B = 0;
+     int*  p = &A;
+     int** q = &p;
+
+     printf("A is %d\n", A);
+     printf("The address of A, '&A' is: %p\n", &A);
+     printf("p is a pointer to A; p is: %p\n", p);
+
+     printf("The address of p, '&p' is: %p\n", &p);
+     printf("q is a pointer to p; q is: %p\n", q);
+
+     // EXPLORE WITH THE DEFERENCING OPERERATOR
+     B = *p;
+     printf("B has the value...%d\n", B);
+
+    
+
+} // MAIN
